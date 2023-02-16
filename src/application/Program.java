babb661bab455188a1e167b15601cb83c0181189
Jordan Araujo <jordandearaujo@outlook.com>
2023-02-15 21:57:57 -0300

    Seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
+import java.text.ParseException;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
-		Department dt = new Department(1, "books");
-		System.out.println(dt);
-
+	public static void main(String[] args) throws ParseException {
+		Department obj = new Department(1, "books");
+		System.out.println(obj);
+		
+		Seller seller = new Seller(1, "joao", "joao@gmail.com", new Date() , 9000.00, obj);
+		System.out.println(seller);
 	}
 }
