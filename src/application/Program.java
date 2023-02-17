75d301a527d39fa2151512ce93651573b4bf32ff
Jordan Araujo <jordandearaujo@outlook.com>
2023-02-17 16:07:32 -0300

    findById impl

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,19 @@
 package application;
 
 import java.text.ParseException;
-import java.util.Date;
 
-import model.entities.Department;
+import model.dao.DAOFactory;
+import model.dao.SellerDAO;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
-		Department obj = new Department(1, "books");
-		System.out.println(obj);
 		
-		Seller seller = new Seller(1, "joao", "joao@gmail.com", new Date() , 9000.00, obj);
+		SellerDAO sellerDao = DAOFactory.createSellerDAO();
+		
+		Seller seller = sellerDao.findById(3);
+		
 		System.out.println(seller);
 	}
 }
